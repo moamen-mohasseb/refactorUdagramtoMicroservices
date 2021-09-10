197c90ecbd1aa322ff05412ac6006dc519ebdb85
moamen_mohasseb <moamen_mohasseb@yahoo.com>
2021-09-10 18:05:06 +0200

    Saving backend before docker image

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=moamen
-export POSTGRES_PASSWORD=12345678
+export POSTGRES_PASSWORD=asd123456
 export POSTGRES_HOST=udb.ccybcreluxi2.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::udagram-369219254633-dev
+export AWS_BUCKET=udagram-369219254633-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=hello
