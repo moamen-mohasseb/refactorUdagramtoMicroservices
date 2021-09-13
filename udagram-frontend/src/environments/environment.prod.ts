505cc1f4ec4b8833da9802c93e6ed0b2cea46a64
moamen_mohasseb <moamen_mohasseb@yahoo.com>
2021-09-13 14:33:30 +0200

    kubctl new front end image

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a14be0034b431406697a399b5f461f25-829125574.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
