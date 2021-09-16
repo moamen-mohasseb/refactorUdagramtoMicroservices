3d0db914bec3198671ce3357cf17fb8611828af5
moamen_mohasseb <moamen_mohasseb@yahoo.com>
2021-09-16 15:23:24 +0200

    update server metrics finish

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=moamen
 export POSTGRES_PASSWORD=asd123456
-export POSTGRES_HOST=database-1.cjj9etslhuzd.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=database-1.czzp8uhjhbbx.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=my-udagram-refactor
-export AWS_REGION=us-east-1
+export AWS_BUCKET=my-s3bucket-udagram
+export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=hello
 export URL=http://localhost:8100
